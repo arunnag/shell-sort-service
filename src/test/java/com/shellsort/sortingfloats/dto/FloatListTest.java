6c5f26755a7581d6c74265a4c42a4ac12c567d13
Arun prasath N <aruntendulkar2000@gmail.com>
2025-03-12 11:06:32 +0530

    iteration2: improved testing

--- src/test/java/com/shellsort/sortingfloats/dto/FloatListTest.java
+++ src/test/java/com/shellsort/sortingfloats/dto/FloatListTest.java
@@ -4,9 +4,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.result.FlashAttributeResultMatchersDsl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
@@ -26,11 +30,20 @@ class FloatListTest {
         FloatList floatList2 = new FloatList(floats);
         assertTrue(floatList1.hashCode() == floatList2.hashCode());
 
-        ArrayList<Float> floats2 = new ArrayList<>(Arrays.asList(1f, 2f, 3f));
+        ArrayList<Float> floats2 = new ArrayList<>(Arrays.asList(1f, 2f, 3f, 4f));
         FloatList floatList3 = new FloatList(floats2);
         assertTrue(floatList1.hashCode() != floatList3.hashCode());
-    }
 
+        FloatList emptyFloatList1 = new FloatList(new ArrayList<>());
+        FloatList emptyFloatList2 = new FloatList(new ArrayList<>());
+        assertTrue(emptyFloatList1.hashCode() == emptyFloatList2.hashCode());
+
+        FloatList nullFloatList = new FloatList();
+        assertTrue(nullFloatList.hashCode() != emptyFloatList1.hashCode());
+
+        Float[] floatArray = {1.1f,2.2f,3.3f};
+        assertFalse(floatList1.equals(floatArray));
+    }
 
 
     /**
@@ -57,10 +70,27 @@ class FloatListTest {
      */
     @Test
     void testEquals() {
-        ArrayList<Float> floats = new ArrayList<>(Arrays.asList(1.1f, 2.2f, 3.3f));
-        FloatList floatList1 = new FloatList(floats);
-        FloatList floatList2 = new FloatList(floats);
+        ArrayList<Float> floats1 = new ArrayList<>(Arrays.asList(1.1f, 2.2f, 3.3f));
+        ArrayList<Float> floats2 = new ArrayList<>(Arrays.asList(1.1f, 2.2f, 3.3f));
+        ArrayList<Float> floats3 = new ArrayList<>(Arrays.asList(1.1f,2.2f,3.3f,4.4f));
+        FloatList floatList1 = new FloatList(floats1);
+        FloatList floatList2 = new FloatList(floats2);
+        FloatList floatList3 = new FloatList(floats3);
+
+        assertTrue(floatList1.equals(floatList1));
         assertTrue(floatList1.equals(floatList2));
+        assertFalse(floatList1.equals(floatList3));
+
+        FloatList emptyList = new FloatList(new ArrayList<>());
+        FloatList otherEmptyList = new FloatList(new ArrayList<>());
+        
+        assertFalse(floatList1.equals(emptyList));
+        assertTrue(emptyList.equals(otherEmptyList));
+        
+        FloatList nullFloatList = new FloatList();
+        assertFalse(emptyList.equals(nullFloatList));
+
+        assertFalse(floatList1.equals(null));
     }
 
 
